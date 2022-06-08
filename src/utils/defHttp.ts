2c4232cb801cab93a31763a7b45298308f3b2ccd
温俊斌 <514446373@qq.com>
2022-06-08 23:33:43 +0800

    baseurl

--- src/utils/defHttp.ts
+++ src/utils/defHttp.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const defHttp = axios.create({
-    // baseURL:'/backend'
-    baseURL:'http://rcwonz1wg.hn-bkt.clouddn.com/'
+    baseURL:'/backend'
+    // baseURL:'http://rcwonz1wg.hn-bkt.clouddn.com/'
 })
 
 export default defHttp
