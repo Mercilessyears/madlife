e60c79707dc6ca354d05d89dfc03a51a1142fd73
温俊斌 <514446373@qq.com>
2023-02-25 17:29:12 +0800

    增加dev模式url

--- src/enums/commEnum.ts
+++ src/enums/commEnum.ts
@@ -1,2 +1,2 @@
 export const Base_Url = 'http://rcwonz1wg.hn-bkt.clouddn.com'
-export const TX_COS_URL = 'https://web-project-1301218253.cos.ap-chengdu.myqcloud.com'
+export const TX_COS_URL = import.meta.env.DEV ? 'files/gltf' : 'https://web-project-1301218253.cos.ap-chengdu.myqcloud.com'
